7da0186c191eb7835622e06e45095e474dd1025b
linshuohao <linshuohao@shencom.cn>
2018-11-20 19:25:17 +0800

    'update'

--- src/modules/subpage/shims-window.d.ts
+++
@@ -1,9 +0,0 @@
-import VueRouter from 'vue-router';
-import {Store} from "vuex";
-
-declare global {
-    interface Window {
-        router: VueRouter;
-        store: Store<{}>;
-    }
-}
--- src/modules/subpage/src/main.ts
+++ src/modules/subpage/src/main.ts
@@ -1,7 +1,16 @@
 import routes from './routers/index';
 import module from './store/index';
+import VueRouter from "vue-router";
+import {Store} from "vuex";
 
 const {router, store} = window;
+
+declare global {
+    interface Window {
+        router: VueRouter;
+        store: Store<{}>;
+    }
+}
 
 router.addRoutes(routes);
 store.registerModule('', module);
